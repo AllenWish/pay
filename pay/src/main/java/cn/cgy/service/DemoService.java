1db1fd6005440531d5d50849dda5a9a7a247456e
AllenWish <1697501868@qq.com>
2017-11-21 14:03:32 +0800

    change code and add layui

---
+++ pay/src/main/java/cn/cgy/service/DemoService.java
@@ -0,0 +1,7 @@
+package cn.cgy.service;
+
+/**
+ * Created by Administrator on 2017-11-21.
+ */
+public interface DemoService {
+}
---
+++ pay/src/main/java/cn/cgy/service/impl/DemoServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.cgy.service.impl;
+
+import cn.cgy.service.DemoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by allenwish on 2017-11-21.
+ */
+@Service("demoService")
+public class DemoServiceImpl implements DemoService {
+
+}
