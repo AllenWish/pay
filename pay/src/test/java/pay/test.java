744d5ef91e55fcf8e2015928e85903c85c1612e9
AllenWish <1697501868@qq.com>
2017-11-21 13:54:00 +0800

    delete code

--- pay/src/test/java/pay/test.java
+++ pay/src/test/java/pay/test.java
@@ -1,22 +1,10 @@
 package pay;
 
-import cn.cgy.pojo.Outpay;
-import cn.cgy.pojo.User;
-import cn.cgy.utils.DateUtils;
+
 
 public class test {
 	public static void main(String[] args) {
-		String date = DateUtils.getCurrentTime(DateUtils.date_19_1);
-		System.out.println(date);
-		
-		Integer i = null;
-		
-		Outpay pay = new Outpay();
-		
-		User uer = new User();
-		
-		System.out.println(pay);
-		System.out.println(uer);
+
 		
 	}
 }
